d5ac6f249f05b42e4e3c7b8392e030929e0976e4
tomoya-tandai <tomoya.tandai26@gmail.com>
2020-08-27 20:39:54 +0900

    first-commit

---
+++ src/views/Detail.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <router-link :to="{name:'Member'}">{{id}}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"]
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <router-link :to="{name: 'Member', params:{id: 1}}">1へ進む</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'Member', params:{id: 2}}">2へ進む</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'Member', params:{id: 3}}">2へ進む</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style scoped>
+ul {
+  padding: 0px;
+}
+li {
+  list-style: none;
+}
+</style>
